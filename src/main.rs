efd40ef668b957f1d9989cfe009de6741133e61b
cbreeden <github@u.breeden.cc>
2016-12-31 13:31:13 -0600

    forgot to remove the error

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 #![feature(proc_macro)]
 #[macro_use] extern crate enum_iter;
 
-#[derive(Debug)]
-struct NoDefault;
+// #[derive(Debug)]
+// struct NoDefault;
 
 #[derive(Debug, EnumIterator)]
 pub enum Test {
     A,
-    B(NoDefault),
+    B,
+    C(u32, u32),
+    D { name: String, flag: bool },
+    // E(NoDefault),
 }
 
 fn main() {
